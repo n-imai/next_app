0901dd07250c8c5b0a894537bf2336c44553a683
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-05-04 12:53:02 +0900

    Use link with other page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
+import Link from 'next/link';
+
+const h1 = {
+    fontSize:'72pt',
+    fontWeight:'bold',
+    textAlign:'right',
+    letterSpacing:'-8px',
+    color:'#f0f0f0',
+    margin:'-40px 0px'
+}
+  
+  
+  const p = {
+    margin:'0px',
+    color:'#666',
+    fontSize:'16pt'
+}
+
+
 export default () =><div>
-  <style jsx>{`
-  h1 {
-    font-size:68pt;
-    font-weight:bold;
-    text-align:right;
-    letter-spacing:-8px;
-    color:#f0f0f0;
-    margin:-32px 0px;
-  }
-  p {
-      margin:0px;
-      color:#666;
-      font-size:16pt;
-  }
-  `}</style>
-  <h1>Next.js</h1>
-  <p>Welcome to next.js!</p>
-  <p>これはビルトインCSSによるスタイルです。</p>
+  <h1 style={h1}>Next.js</h1>
+  <p style={p}>Welcome to next.js!</p>
+  <ht />
+  <div>
+    <Link href="/other">
+      <a>Go to Other page &gt;&gt;</a>
+    </Link>
+  </div>
 </div>
