eca2ad2560ae2a4e30e2337bab0b040fa145831b
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-05-05 13:12:58 +0900

    Use style with other file

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,14 @@
 import Link from 'next/link';
-
-const h1 = {
-    fontSize:'72pt',
-    fontWeight:'bold',
-    textAlign:'right',
-    letterSpacing:'-8px',
-    color:'#f0f0f0',
-    margin:'-40px 0px'
-}
-  
-  
-  const p = {
-    margin:'0px',
-    color:'#666',
-    fontSize:'16pt'
-}
-
+import style from '../static/Style'
 
 export default () =><div>
-  <h1 style={h1}>Next.js</h1>
-  <p style={p}>Welcome to next.js!</p>
+  {style}
+  <h1>Next.js</h1>
+  <p>Welcome to next.js!</p>
   <ht />
   <div>
     <Link href="/other">
-      <a>Go to Other page &gt;&gt;</a>
+      <button>Go to Other page &gt;&gt;</button>
     </Link>
   </div>
 </div>
