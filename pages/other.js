71c4915006992d4c3677f40d9c09601e5ec58e81
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-05-05 15:25:21 +0900

    Use counter in index js

--- pages/other.js
+++ pages/other.js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import Counter from '../components/Counter';
 
 export default () => (
   <Layout header="Other" title="Other page.">
-    <p>This is Other page.</p>
+    <Counter />
     <hr/>
-      <Link href="/">
-        <button>&lt;&lt; Back to Top</button>
-      </Link>
+      <div>
+        <Link href="/">
+          <button>&lt;&lt; Back to Top</button>
+        </Link>
+      </div>
   </Layout>
-)
+);
