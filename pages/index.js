7e1e966bf67a9daba9d8cd1ce9dfabbe3cb7ec85
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-05-05 14:09:47 +0900

    Add image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
-import style from '../static/Style';
 import Layout from '../components/Layout';
+import Image from '../static/Image'
 
 export default () => (
   <Layout header="Next" title="Top page.">
     <p>Welcome to next.js!</p>
+    <Image fname="image.jpg" size="250" />
     <ht />
     <Link href="./other"><button>
       go to Other &gt;&gt;
