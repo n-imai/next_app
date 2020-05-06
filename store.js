23b859bbd8c0d52a5ae4d126cb990f472bd3e40e
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-05-06 10:43:47 +0900

    Add calc to store

--- store.js
+++ store.js
@@ -5,27 +5,35 @@ import thunkMiddleware from 'redux-thunk';
 // ステート初期値
 const initial = {
   message:'START',
-  count: 0
+  data:[],
+  number:[],
+  result:0,
 }
 
 
 // レデューサー
-function counterReducer (state = initial, action) {
+function calcReducer (state = initial, action) {
   switch (action.type) {
-    case 'INCREMENT':
-      return {
-        message: 'INCREMENT',
-        count: state.count + 1
-      };
-    case 'DECREMENT':
-      return {
-        message: 'DECREMENT',
-        count: state.count - 1
-      };
+    case 'ENTER':
+      let data2 = state.data.slice();
+      let s = action.value;
+      data2.unshift(s);
+      let num = s.replace(/[^0-9]/g,"");
+      let number2 = state.number.slice();
+      number2.unshift(num);
+      let result = (state.result * 1) + (num * 1);
+      return {
+        message: 'ENTER',
+        data: data2,
+        number: number2,
+        result: result
+      };
     case 'RESET':
       return {
         message: 'RESET',
-        count: initial.count
+        data:[],
+        number:[],
+        result:0,
       };
     default:
       return state;
@@ -35,6 +43,6 @@ function counterReducer (state = initial, action) {
 
 // initStore関数（redux-store.jsで必要）
 export function initStore(state = initial) {
-  return createStore(counterReducer, state,
+  return createStore(calcReducer, state,
     applyMiddleware(thunkMiddleware))
 }
