2970704e180f42c53baf923010ec1874b60d2a19
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-05-06 11:38:48 +0900

    Fix bug

--- pages/calc.js
+++ pages/calc.js
@@ -2,14 +2,14 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import Calc from '../components/Calc';
 
-export default () =>{
+export default () => (
   <Layout header="Calc" title="calculator">
     <Calc />
     <hr />
     <div>
       <Link href="/">
-        <button>&lt;&le; Back to Top</button>
+        <button>&lt;&lt; Back to Top</button>
       </Link>
     </div>
   </Layout>
-};
+);
